dfa581925fdbf163bc3b8f2757ce87734f777901
Anjas Kurniawan <anjaskurniawan737@gmail.com>
2022-03-27 03:36:28 +0700

    animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,14 +12,24 @@ module.exports = {
         100: "600px",
       },
       animation: {
-        "spin-slow": "spin 3s linear infinite",
-        "ping-slow": "ping 3s cubic-bezier(0,0,0.2,0) infinite",
-        "blink-slow": "blink 3s linear infinite",
+        show: "show 2s linear",
+        swipekiri: "swipekiri 1s ease-in alternate",
+        swipeshow: "swipeshow 1s ease-in",
+        "spin-slow": "spin 5s infinite linear",
       },
       keyframes: {
-        blink: {
-          "0%,100%": { opacity: "0" },
-          "50%": { opacity: "1" },
+        show: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        swipeshow: {
+          "0%": { opacity: "0", transform: "translateX(-200px)" },
+          "100%": { opacity: "1", transform: "translateX(0)" },
+        },
+        swipekiri: {
+          from: { margin: "left(250px)", width: "300px" },
+          "75%": { margin: "left(100px)", width: "150%" },
+          to: { margin: "left(0)", width: "100%;" },
         },
       },
     },
