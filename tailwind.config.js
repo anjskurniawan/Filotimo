35a98993fff8a33361f58c5fc7801ab363eea535
Anjas Kurniawan <anjaskurniawan737@gmail.com>
2022-03-27 01:55:03 +0700

    services add

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,13 @@ module.exports = {
       animation: {
         "spin-slow": "spin 3s linear infinite",
         "ping-slow": "ping 3s cubic-bezier(0,0,0.2,0) infinite",
+        "blink-slow": "blink 3s linear infinite",
+      },
+      keyframes: {
+        blink: {
+          "0%,100%": { opacity: "0" },
+          "50%": { opacity: "1" },
+        },
       },
     },
   },
